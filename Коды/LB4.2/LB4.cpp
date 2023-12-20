d54febe374b283a578a6e3e5c7313352c7753a8c
Spongebob <mistergd2281339@gmail.com>
2023-12-20 13:27:50 +0700

    LB4

--- Коды/LB4.2/LB4.cpp
+++ Коды/LB4.2/LB4.cpp
@@ -36,44 +36,61 @@ double CalculateAverage(const vector<Student>& students, const string& group, in
 }
 
 // Функция для обработки данных без многопоточности
-double ProcessWithoutMultithreading(const vector<Student>& students, const string& group, int semester) {
+pair<double, double> ProcessWithoutMultithreading(const vector<Student>& students, const string& group, int semester) {
     auto start = chrono::high_resolution_clock::now();
     double average = CalculateAverage(students, group, semester);
     auto end = chrono::high_resolution_clock::now();
     chrono::duration<double, milli> duration = end - start;
-    cout << "Время обработки без использования многопоточности: " << duration.count() << " мс\n";
-    return average;
+    return make_pair(average, duration.count());
 }
 
 // Глобальный мьютекс для синхронизации вывода
 mutex output_mutex;
 
 // Функция для обработки данных в отдельном потоке
-void ThreadFunction(const vector<Student>& students, const string& group, int semester, double& average) {
+void ThreadFunction(const vector<Student>& students, const string& group, int semester, pair<double, double>& result) {
     auto start = chrono::high_resolution_clock::now();
-    average = CalculateAverage(students, group, semester);
+    double average = CalculateAverage(students, group, semester);
     auto end = chrono::high_resolution_clock::now();
     chrono::duration<double, milli> duration = end - start;
     lock_guard<mutex> lock(output_mutex);
-    cout << "Время обработки с использованием многопоточности: " << duration.count() << " мс\n";
+    result = make_pair(average, duration.count());
 }
 
 int main() {
     setlocale(LC_ALL, "Rus");
     vector<Student> students = {
         // Пример данных о студентах
-        {"Иванов Иван Иванович", "Г1", {{1, "Математика", 4}, {1, "Физика", 5}}},
+        {"Иванов Иван Иванович1", "Г1", {{1, "Математика", 4}, {1, "Физика", 5}}},
+        {"Иванов Иван Иванович2", "Г1", {{1, "Математика", 4}, {1, "Физика", 5}}},
+        {"Иванов Иван Иванович3", "Г1", {{1, "Математика", 4}, {1, "Физика", 5}}},
     };
 
     // Вызов функции без многопоточности
-    double average = ProcessWithoutMultithreading(students, "Г1", 1);
-    cout << "Средняя успеваемость студентов группы Г1 за семестр 1: " << average << "\n";
+    pair<double, double> result = ProcessWithoutMultithreading(students, "Г1", 1);
+    cout << "Время обработки без использования многопоточности: " << result.second << " мс\n";
+    cout << "Средняя успеваемость студентов группы Г1 за семестр 1: " << result.first << "\n";
 
     // Многопоточная обработка
-    double threaded_average = 0;
-    thread worker(ThreadFunction, ref(students), "Г1", 1, ref(threaded_average));
-    worker.join();
-    cout << "Средняя успеваемость студентов группы Г1 за семестр 1 (многопоточность): " << threaded_average << "\n";
+    int num_threads = 16; // количество потоков
+    vector<thread> workers;
+    vector<pair<double, double>> threaded_results(num_threads);
+    for (int i = 0; i < num_threads; ++i) {
+        workers.push_back(thread(ThreadFunction, ref(students), "Г1", 1, ref(threaded_results[i])));
+    }
+    for (auto& worker : workers) {
+        worker.join();
+    }
+    double total_average = 0;
+    double total_time = 0;
+    for (const auto& res : threaded_results) {
+        total_average += res.first;
+        total_time += res.second;
+    }
+    total_average /= num_threads;
+    total_time /= num_threads;
+    cout << "Время обработки с использованием многопоточности: " << total_time << " мс\n";
+    cout << "Средняя успеваемость студентов группы Г1 за семестр 1 (многопоточность): " << total_average << "\n";
 
     return 0;
 }
